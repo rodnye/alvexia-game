a75a5be704af9ef393c4bfc6dd23d9e6f6d24d60
RodnyE <rrodnyestrada01@gmail.com>
2022-02-09 15:17:48 -0500

    fixed bug

--- config.js
+++ config.js
@@ -1,28 +1,42 @@
 //propiedades globales
 config = {
-  
-  APP_TOKEN: "La_6362kwjsbd&uwueb277291",
-  PACKAGE: app.GetPackageName(),
-  VERSION: "0.0.1",
-  
-  URL: {
-    server: "https://alvexia-server-franky96lol.dcoder.run",
-    socket: "",
-    auth: this.URL.server+"/auth/login",
-    register: this.URL.server+"/auth/register",
-    wake_up: this.URL.server+"/wakeup"
-  },
-  PATH: {
-    img: "./Img",
-    sound: "./Snd",
-    lib: "./Html",
-    script: "./script",
-    data: "/sdcard/Android/data/"+this.PACKAGE,
-  },
-  USER: {
-    token: null,
-    name: null,
-    password: null
+      APP_TOKEN: "La_6362kwjsbd&uwueb277291",
+      PACKAGE: app.GetPackageName(),
+      VERSION: "0.0.1",
+
+      URL: {
+        server: "https://alvexia-server-franky96lol.dcoder.run",
+        socket: "",
+        auth: "{{URL.server}}" + "/auth/login",
+        register: "{{URL.server}}" + "/auth/register",
+        wake_up: "{{URL.server}}" + "/wakeup"
+      },
+      PATH: {
+        img: "./Img",
+        sound: "./Snd",
+        lib: "./Html",
+        script: "./script",
+        data: "/sdcard/Android/data/"+"{{PACKAGE}}",
+      },
+      USER: {
+        token: null,
+        name: null,
+        password: null
+      }
+}
+
+//function parseadora de string
+function pp(c){
+  let isObj = (v)=>(typeof v == "object" && !Array.isArray(v) && typeof v!=null);
+  for(let i in c){
+    if(isObj(c[i])) pp(c[i]);
+    else if(typeof c[i] == "string") {
+      let syntax = /\{\{(.*)?\}\}/;
+      let value = ()=>{
+        if(syntax.test(c[i])) return eval("config."+syntax.exec(c[i])[1]);
+        else return "";
+      }
+      c[i] = c[i].replace(syntax, value())
+    }
   }
-};
-//app.ShowPopup("loaded config", "bottom");
+} pp(config);
